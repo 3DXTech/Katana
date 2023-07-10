70ddd4f2261e9ce862097998d8751a785379a8f1
ericedson98 <121807834+ericedson98@users.noreply.github.com>
2023-07-10 15:33:53 -0400

    Change slicer name to Katana

---
+++ build/src/libslic3r/libslic3r_version.h
@@ -0,0 +1,9 @@
+#ifndef __SLIC3R_VERSION_H
+#define __SLIC3R_VERSION_H
+
+#define SLIC3R_APP_NAME "Katana"
+#define SLIC3R_APP_KEY "Katana"
+#define SLIC3R_VERSION "2.6.0"
+#define SLIC3R_BUILD_ID "Katana-2.6.0"
+
+#endif /* __SLIC3R_VERSION_H */
