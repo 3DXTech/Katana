1b7522bcda81dcb63cbaa2936ed43235ce897429
Eric Edson <ete@3dxtech.com>
2024-02-07 10:02:41 -0500

    Changed version number

--- build/src/libslic3r/libslic3r_version.h
+++ build/src/libslic3r/libslic3r_version.h
@@ -3,7 +3,7 @@
 
 #define SLIC3R_APP_NAME "KatanaSlicer"
 #define SLIC3R_APP_KEY "KatanaSlicer"
-#define SLIC3R_VERSION "2.7.1"
-#define SLIC3R_BUILD_ID "KatanaSlicer-2.7.1+UNKNOWN"
+#define SLIC3R_VERSION "1.0.0"
+#define SLIC3R_BUILD_ID "KatanaSlicer-1.0.0"
 
 #endif /* __SLIC3R_VERSION_H */
